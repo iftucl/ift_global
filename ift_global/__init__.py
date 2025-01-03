3b6ffedded56d52ffc2be07ac24c1979d7fecf81
uceslc0 <uceslc0@gmail.com>
2025-01-03 10:05:22 +0000

    adding first commit

---
+++ ift_global/__init__.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+import importlib
+import typing
+
+__version__ = "0.0.0"
+__version_tuple__ = (0, 0, 0)
+
+__doc__ = """
+ift_global
+=======================================================================
+
+
+
+"""
+
+
+__all__ = []
